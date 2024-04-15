c9707109728e5e684c1b3f1fe36247856dc01a8b
Ankit Chauhan <112958248+ankitchauhan8931@users.noreply.github.com>
2024-04-15 17:17:47 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);
+
+// console.log("2" > 2);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//same as undefined
+
+// ===  check the datatype of both the element.
+ console.log("2" === 2);
+

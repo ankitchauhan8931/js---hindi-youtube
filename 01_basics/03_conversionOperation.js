9ac57ca0973a62686bac40035550f97e762961e9
Ankit Chauhan <112958248+ankitchauhan8931@users.noreply.github.com>
2024-04-15 17:04:15 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn= 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 //1 =>true; 0=>false;
@@ -22,5 +22,31 @@ console.log(booleanIsLoggedIn);
 
 let someNumber= 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************** Operations ***************
+
+
+let value=3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+
+let tr1="Hello"
+let str2= " Ankit"
+let str3 = tr1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log("1"+(2+2));
+
+console.log(+true);
